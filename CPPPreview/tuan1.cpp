09ed58caf29c360008e8bfd5ab81beb86d3bf1e9
Nguyen Tuan Kiet <nguyentuankiet23031998@gmail.com>
2017-09-28 10:25:30 +0700

    bai cua ban HAO

--- CPPPreview/tuan1.cpp
+++ CPPPreview/tuan1.cpp
@@ -1,9 +1,66 @@
-#include<iostream>
+#include<stdio.h>
 #include<conio.h>
+struct game
+{
+	int ma;
+	char ten[50];
+	float tien;
+	char tacgia[100];
+};
+void them1maychoigame(game a[], int &n)
+{
+	printf("nhap ma game: ");
+	scanf_s("%d", &a[n].ma);
 
-using namespace std;
+	printf("nhap ten: ");
+	gets_s(a[n].ten);
 
+	printf("nhap gia tien: ");
+	scanf_s("%f", &a[n].tien);
+
+	printf("nhap ten tac gia: ");
+	gets_s(a[n].tacgia);
+}
+void themnhieumaychoigame(game a[], int &n)
+{
+	int m;
+	printf("ban muon them bao nhieu may choi game: ");
+	scanf_s("%d", &m);
+	for (int i = 0; i < n; i++)
+	{
+		printf("nhap ma game: ");
+		scanf_s("%d", &a[n].ma);
+
+		printf("nhap ten: ");
+		gets_s(a[n].ten);
+
+		printf("nhap gia tien: ");
+		scanf_s("%f", &a[n].tien);
+
+		printf("nhap ten tac gia: ");
+		gets_s(a[n].tacgia);
+	}
+}
 void main() {
-	cout << "Hello World" << endl;
-	_getch();
+	game a;
+	int a = , n;
+	int chon = 1, tieptuc;
+	do
+	{
+		printf("-------menu------\n");
+		printf("them 1 may choi game");
+		printf("them nhieu may choi game");
+		printf("chon chuc nang: ");
+		scanf_s("%d", chon);
+		switch (tieptuc)
+		{
+		case 0:return;
+		case 1:them1maychoigame(a, n);
+			break;
+		case 2:
+			break;
+		default:
+			break;
+		}
+	} while (chon != 0);
 }
